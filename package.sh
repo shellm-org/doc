9bde931cdd9a4a977668f545b49a41554e5eaa8b
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-12-05 00:33:45 +0100

    Stop trying to install shellman

--- package.sh
+++ package.sh
@@ -2,9 +2,9 @@ BINS="bin/doc"
 DEPS="gitlab.com/shellm/core"
 
 # Do not install shellman if it is already available
-if ! command -v shellman &>/dev/null; then
-  DEPS="${DEPS}:pawamoy/shellman"
-fi
+# if ! command -v shellman &>/dev/null; then
+#   DEPS="${DEPS}:pawamoy/shellman"
+# fi
 
 BASH_COMPLETIONS="cmp/doc.completion.bash"
 ZSH_COMPLETIONS="cmp/doc.completion.zsh"
